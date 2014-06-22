8fafc5705880c5f50005231d0195274fb34c3e39
Manuel A. Bolvivar <ma.bolivar643@gmail.com>
2014-06-22 11:42:07 -0500

    Structured R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,25 @@
 #===============================================
 # Working directory
 #===============================================
-setwd("C:\\Users\\manuel\\Dropbox\\Coursera\\Data Science Specialization\\Getdata\\Project")
+setwd("C:/Users/Manuel/Dropbox/3. DSS/Wearable-computing---data-cleaning")
+#===============================================
+
+#===============================================
+# Libraries
 #===============================================
 library(reshape2)
+#===============================================
 
+#===============================================
+# Script
+#===============================================
+
+
+#0. Buld directories
 testdir <- "./UCI HAR Dataset/test"
 traindir <- "./UCI HAR Dataset/train"
 
-#1. Read files
-testfiles <- dir(testdir,pattern = "[:print:][.txt]")
-trainfiles <- dir(traindir,pattern = "[:print:][.txt]")
-
-meltdata <- data.frame() #Final data frame
-
-## 1.1 Read id, activity & features files
+## 1 Read id, activity & features files
 idtest <- read.table(paste(testdir,"/subject_test.txt", sep=""))
 idtrain <- read.table(paste(traindir,"/subject_train.txt", sep=""))
 activitytest <- read.table(paste(testdir,"/y_test.txt", sep=""))
@@ -22,20 +27,20 @@ activitytrain <-  read.table(paste(traindir,"/y_train.txt", sep=""))
 featurestest <- read.table(paste(testdir,"/X_test.txt", sep=""))
 featurestrain <-  read.table(paste(traindir,"/X_train.txt", sep=""))
 
-#Build features data set
+#2. Build features data set
 features <- rbind(cbind(idtest,activitytest,featurestest),cbind(idtrain,activitytrain,featurestrain))
 
 con <- file("./UCI HAR Dataset/features.txt")
 featurenames <- readLines(con)
 close(con)
 
-#Extract the mean stdv measurements
+#3. Extract the mean and stdv measurements
 featurenames <- unlist(strsplit(featurenames," "))[seq(2,length(featurenames)*2,2)]
 names(features) <- c("ParticipantID","Activity", featurenames)
 features <- features[order(features$ParticipantID,features$Activity),]
 features <- features[,c(T,T,(grepl("mean",tolower(featurenames))) | grepl("std",tolower(featurenames)))]
 
-# Add activity labels
+#4. Add activity labels
 acdata <- features
 con <- file("./UCI HAR Dataset/activity_labels.txt")
 activity_labels <- readLines(con)
@@ -48,7 +53,7 @@ acdata <- acdata[order(acdata$ParticipantID,acdata$Activity),]
 acdata$Activity <- acdata$ActivityLabel
 acdata$ActivityLabel <- NULL
 
-#Export data file
+#5. Export data file
 write.table(acdata,"tidy_accelerometry_data.txt",row.names=F)
 
 #Second data file
@@ -57,3 +62,5 @@ str(melted)
 
 summaryData <- dcast(melted,ParticipantID + Activity  ~  variable,mean)
 write.table(summaryData,"summary_data_mean_stdv.txt",row.names=F)
+
+#===============================================
